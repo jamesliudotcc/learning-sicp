de98d2895408c1b42709a7134db571beba230c73
James Liu <james@pop-os.localdomain>
2019-09-14 21:02:32 -0700

    added ex16-17

---
+++ ch1/ex17.scm
@@ -0,0 +1,5 @@
+;; Now suppose we include, together with addition, operations double,
+;; which doubles an integer, and halve, which divides an (even) integer
+;; by 2. Using these, design a multiplication procedure analogous to
+;; fast-expt that uses a logarithmic number of steps. 
+
